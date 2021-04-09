93b6e5488fe97cdc079728ff961e7db451e92b1a
Gavin Lanata <gavinlanata@hotmail.com>
2021-04-09 09:03:38 +0100

    check for mod on command

--- obsbot/scripts/twitch.js
+++ obsbot/scripts/twitch.js
@@ -50,6 +50,12 @@ client.on('message', (channel, tags, message, self) => {
     const args = message.slice(1).split(' ');
     const command = args.shift().toLowerCase();
 
+    let isMod = user.mod || user['user-type'] === 'mod';
+    let isBroadcaster = channel.slice(1) === user.username;
+    let isModUp = isMod || isBroadcaster;
+
+    if (!isModUp) return;
+
     //if (command === 'echo') {
     //    client.say(channel, `@${tags.username}, you said: "${args.join(' ')}"`);
     //}
